81558fd211fa87f1e3dae77ed7eb07ba6a59b626
Alvaro Garcia <derecerca@gmail.com>
2016-02-22 17:15:50 +0100

    refactor: convert to LTR, TTB

--- src/kata/checkout/core.clj
+++ src/kata/checkout/core.clj
@@ -28,16 +28,20 @@
                           (print-help options)
                           (println (:errors options)))
       (help? options) (print-help options)
-      :else (let [rules-path (get-in options [:options :rules])
-                  rules-content (slurp rules-path)
-                  config (eval (read-string rules-content))
+      :else (let [config (->> [:options :rules]
+                       (get-in options)
+                       slurp
+                       read-string
+                       eval)
 
                   find-by-name #(= (:name %) :side-effectful)
-                  rules (:rules config)
                   selector (:selector config)
-                  matching-rules (filter find-by-name rules)
-                  selected-rules (selector matching-rules)
-                  rule-code (map :expr selected-rules)]
+
+                  matching-rules (->> config
+                       :rules
+                       (filter find-by-name)
+                       selector
+                       (map :expr))]
               ;(println options)
               (println "doing stuff")
-              (execute rule-code)))))
+              (execute matching-rules)))))
